c134aa633f894e7db91202a2fee32d7748acd543
hira0912 <hira0912@gmail.com>
2024-09-01 17:09:36 +0900

    update

--- sudoku02.py
+++ sudoku02.py
@@ -1,12 +1,15 @@
 def lambda_handler(event, context):
-    # 盤面の設定
-    panel_first = ['x91x65x7x','xx6xx48x1','xx5817x26','xxxxxxxxx','162x89xx4','4xx1xx68x','25xxx819x','6xxx513x2','xxx472xxx']
+    
+    # 問題盤面（及び答え）の設定
+    panel_question = ['x91x65x7x','xx6xx48x1','xx5817x26','xxxxxxxxx','162x89xx4','4xx1xx68x','25xxx819x','6xxx513x2','xxx472xxx']
+    panel_question_ans = ['891265473','726394851','345817926','538746219','162589734','479123685','254638197','687951342','913472568']
     panel_difficult = ['8xxxxxxxx','xx36xxxxx','x7xx9x2xx','x5xxx7xxx','xxxx457xx','xxx1xxx3x','xx1xxxx68','xx85xxx1x','x9xxxx4xx']
-    #panel_last = ['891265473','726394851','345817926','538746219','162589734','479123685','254638197','687951342','913472568']
+    panel_diffucult_ans = ['812753649','943682175','675491283','154237896','369845721','287169534','521974368','438526917','796318452']
+
     
     # 盤面の計算
-    panel_str = ''.join(panel_first)
-    panel_ans_str = calc_panel(panel_str,0)
+    panel_str = ''.join(panel_question)
+    ret,panel_ans_str = calc_panel(panel_str,0)
     
     # 計算結果の表示
     panel_ans = []
@@ -16,17 +19,32 @@ def lambda_handler(event, context):
 
 # 計算ルーチン
 def calc_panel(panel,position):
+    # セルが最終セルの場合
+    if(position>80):
+        return check_panel(panel),panel
+
+    # 変数の定義
+    ret = False
+    panel_ans = []
+
+    # セルが空欄の場合
     if(panel[position]=='x'):
-        possibility = calc_possibility(panel,position) #[1,2,3,4,5,6,7,8,9]
-        while len(possibility)>0 :
-            panel_mod = setval(panel,position,possibility.pop(0))
-            ret = calc_panel(panel_mod,position+1)
-    else:
-        ret = calc_panel(panel,position+1)
-    
-    if not ret:
+        possibility = calc_possibility(panel,position) # possibility = [1,2,3,4,5,6,7,8,9]
         
-                    
+        while len(possibility)>0 :
+            # 答え導出完了の為の終了条件
+            if ret:
+                return ret,panel_ans
+            # セルに一つ埋めて渡す
+            panel_mod = setval(panel,position,possibility.pop(0))
+            ret,panel_ans = calc_panel(panel_mod,position+1)
+        return ret,panel_ans
+
+    # セルが既に埋まっている場合は下のセルに継ぐ
+    else:
+        ret,panel_ans = calc_panel(panel,position+1)
+        return ret,panel_ans
+
 # セル単位の配置可能性の計算
 def calc_possibility(panel,position):
     possibility = ['1','2','3','4','5','6','7','8','9']
